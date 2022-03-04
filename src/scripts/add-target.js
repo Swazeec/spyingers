8d7f1e96db32894a2202617924506e3f7dcb5685
Soizic Venel <venel.soizic@hotmail.fr>
2022-03-04 18:37:58 +0100

    add add-target.js

---
+++ src/scripts/add-target.js
@@ -0,0 +1,54 @@
+let firstname = document.getElementById('firstname')
+let lastname = document.getElementById('lastname')
+let codename = document.getElementById('codename')
+let birthDate = document.getElementById('birthDate')
+let nationality = document.getElementById('nationality')
+let submitBtn = document.getElementById('submit')
+let error =''
+let regx = /[^a-zA-ZÀ-ž\-\'\ ]/mg
+
+function verifyFirstname(value){
+    if(value =='' || value.length > 50 || regx.test(value)){
+        error += 'Prénom invalide.\n'
+    }
+}
+
+function verifyLastname(value){
+    if(value =='' || value.length > 50 || regx.test(value)){
+        error += 'Nom invalide.\n'
+    }
+}
+
+function verifyCodename(value){
+    if(value =='' || value.length > 50){
+        error += 'Nom de code invalide.\n'
+    }
+}
+function verifyBirthdate(value){
+    let birth = new Date(value).getTime()
+    let now = new Date().getTime()
+    if(isNaN(birth)){
+        error += 'Format de date invalide.\n'
+    } else if(now < birth){
+        error += 'Date invalide.\n'
+    }
+}
+
+function verifyNationality(value){
+    if(isNaN(value) || value < 1){
+        error += 'Nationalité invalide.\n'
+    }
+}
+
+submitBtn.addEventListener('click', function(event){
+    verifyFirstname(firstname.value)
+    verifyLastname(lastname.value)
+    verifyCodename(codename.value)
+    verifyBirthdate(birthDate.value)
+    verifyNationality(nationality.value)
+    if(error !==''){
+        alert(error)
+        error = ''
+        event.preventDefault()
+    } 
+})
